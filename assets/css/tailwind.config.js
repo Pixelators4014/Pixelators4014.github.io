63754afa3026438767bd1208139b73528237b540
Ashwin Naren <arihant2math@gmail.com>
2024-05-31 21:55:00 -0700

    [UI] Custom Colors

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -4,7 +4,34 @@ const rootDir = __dirname + '/../../';
 module.exports = {
     content: [`${rootDir}/layouts/**/*.html`, `${rootDir}/content/**/*.md`, `${rootDir}/themes/**/*.html`],
     darkMode: 'selector',
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
+    plugins: [],
+    theme: {
+        colors: {
+            'ctheme': {
+                'light': '#ffffff',
+                DEFAULT: '#ffffff',
+                'dark': '#1d1e20',
+            },
+            'centry': {
+                'light': '#ffffff',
+                DEFAULT: '#ffffff',
+                'dark': '#2e2e33',
+            },
+            'cprimary': {
+
+            },
+            'csecondary': {
+
+            },
+            'ctertiary': {
+
+            },
+            'ccontent': {
+                'light': '#1f1f1f',
+                DEFAULT: '#1f1f1f',
+                'dark': '#c4c4c5',
+            },
+
+        }
+    }
 }
--- assets/js/fastsearch.js
+++ assets/js/fastsearch.js
@@ -88,8 +88,8 @@ sInput.onkeyup = function (e) {
             let resultSet = ''; // our results bucket
 
             for (let item in results) {
-                resultSet += `<li class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"><header class="entry-header">${results[item].item.title}&nbsp;»</header>` +
-                    `<a href="${results[item].item.permalink}" aria-label="${results[item].item.title}"></a></li>`
+                resultSet += `<li><a class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white" href="${results[item].item.permalink}" aria-label="${results[item].item.title}"><header class="entry-header">${results[item].item.title}&nbsp;»</header>` +
+                    `</a></li>`
             }
 
             resList.innerHTML = resultSet;
