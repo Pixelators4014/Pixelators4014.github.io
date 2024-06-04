c9d182c10bcead3e77aac802883cb18236b374ae
Ashwin Naren <arihant2math@gmail.com>
2024-06-03 19:33:40 -0700

    chore: switched to webpack for tailwind intersect

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: {
         'turbo': './src/turbo.js',
+        'intersect': './src/intersect.js',
     },
     output: {
         path: path.resolve(__dirname, 'assets/js/'),
