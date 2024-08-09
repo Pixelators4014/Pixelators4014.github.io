ec274466dd8638258d1988cd63c8c9fe9fae7cd3
Ashwin Naren <ashwin@pixelators.org>
2024-08-09 10:05:42 -0700

    feat: sentry support

--- src/sentry.js
+++ src/sentry.js
@@ -19,5 +19,5 @@ Sentry.init({
     // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
     // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
     // results in 25% of transactions being profiled (0.5*0.5=0.25)
-    profilesSampleRate: 1.0,
+    profilesSampleRate: 0.75,
 });
