c7d31be53eccf4911f72c0cb3102743586fce297
Ashwin Naren <ashwin@pixelators.org>
2024-08-09 09:57:56 -0700

    feat: sentry support

--- src/sentry.js
+++ src/sentry.js
@@ -4,13 +4,20 @@ Sentry.init({
     dsn: "https://38ed2e5ebef8f81da02881729196eede@o4507371853512704.ingest.us.sentry.io/4507371856986112",
     integrations: [
         Sentry.browserTracingIntegration(),
+        Sentry.browserProfilingIntegration(),
         Sentry.replayIntegration(),
     ],
     // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    tracesSampleRate: 0.5, //  Capture 100% of the transactions
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    tracePropagationTargets: ["localhost", /^https:\/\/pixelators\.org/],
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    // Set profilesSampleRate to 1.0 to profile every transaction.
+    // Since profilesSampleRate is relative to tracesSampleRate,
+    // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+    // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+    // results in 25% of transactions being profiled (0.5*0.5=0.25)
+    profilesSampleRate: 1.0,
 });
